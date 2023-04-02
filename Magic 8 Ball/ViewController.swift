1a31c253f3dd6bd390c7f51c2332b83feb51946c
Rajat Nagvenker <53140446+R14801@users.noreply.github.com>
2023-04-02 23:11:23 +0530

    Final Commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,8 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
+    @IBOutlet weak var magicBallImage: UIImageView!
+    
+    @IBAction func shakeButtonPressed(_ sender: UIButton) {
+        magicBallImage.image = ballArray.randomElement()
+    }
+    
 
 }
 
